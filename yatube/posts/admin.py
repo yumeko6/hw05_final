ebc17fc4820dbc5088279ab80859b57f52271aa7
Виктор Тузовский <vtuzovskiy@fura.ru>
2022-03-11 12:46:01 +0300

    First review changes - added PostComment

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Group
+from .models import Post, Group, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -16,5 +16,20 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class PostComment(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'post',
+    )
+    list_editable = ('text',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
+admin.site.register(Comment, PostComment)
